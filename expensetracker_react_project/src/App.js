985f032dfa1a134048ca7611296d5674caf53131
RubaHashash <ruba.hashash@gmail.com>
2020-11-17 21:42:15 +0200

    add category form

--- expensetracker_react_project/src/App.js
+++ expensetracker_react_project/src/App.js
@@ -9,6 +9,8 @@ import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Expenses from './pages/Expenses';
+import AddCategory from './pages/AddCategory';
+import Logout from './pages/Logout';
 
 
 function App() {
@@ -18,9 +20,12 @@ function App() {
       
       <Router>
         <Route path="/" exact component={Login}/>
-        <Route path="/login" exact component={Login}/>
+        <Route path="/Login" exact component={Login}/>
+        <Route path="/Logout" exact component={Logout}/>
         <Route path="/Register" exact component={Register}/>
         <Route path="/Expenses" exact component = {Expenses}/>
+        <Route path="/AddCategory" exact component = {AddCategory}/>
+
       </Router>
 
     </div>
