e36054bda07dce119c59b7dcda2d51bcd6f76a8d
RubaHashash <ruba.hashash@gmail.com>
2020-11-22 01:24:11 +0200

    add data to the pie chart

--- expensetracker_react_project/src/pages/Expenses.js
+++ expensetracker_react_project/src/pages/Expenses.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import Header from './Header';
+import PieChart from './PieChart';
 import {Redirect} from 'react-router-dom';
-
+import Pagination from 'react-paginate';
 
 
 class Expenses extends Component{
@@ -11,14 +12,71 @@ class Expenses extends Component{
         super(props)
         this.state={
             expenses_list: [],
+            flag: false,
+            sort: (a, b) => a.id < b.id ? 1 : -1,
+            chartData: []
+            // activePage: 1,
+            // itemsCountPerPage: 1,
+            // totalItemsCount: 1,
+            // pageRangeDisplayed: 1
         }
+        // this.handlePageChange = this.handlePageChange.bind(this);
     }
 
     componentDidMount(){
         let id = localStorage.getItem('id');
         axios.get('/api/expense/'+id)
         .then(response=>{
-            this.setState({expenses_list:response.data});
+            this.setState({
+                expenses_list:response.data,
+                // itemsCountPerPage: response.data.per_page,
+                // totalItemsCount: response.data.total,
+                // activePage: response.data.current_page
+            });
+        });
+
+        this.getChart();
+    }
+
+    handleAmountSort = () => {
+        
+        if (this.state.sort== "(a, b) => a.amount > b.amount ? 1 : -1")
+        {
+            this.setState({ sort: (a, b) => a.amount < b.amount ? 1 : -1 });
+        }
+        else {
+            this.setState({ sort: (a, b) => a.amount > b.amount ? 1 : -1 });
+        }
+       
+    }
+    handleDateSort = () => {
+       if (this.state.sort== "(a, b) => a.date > b.date ? 1 : -1")
+        {
+            this.setState({ sort: (a, b) => a.date < b.date ? 1 : -1 });
+        }
+        else {
+            this.setState({ sort: (a, b) => a.date > b.date ? 1 : -1 });
+        }
+      
+  }
+
+
+    // handlePageChange(pageNumber = 1){
+    //     let id = localStorage.getItem('id');
+    //     axios.get('/api/expense/'+id+'?page='+pageNumber)
+    //     .then(response=>{
+    //         this.setState({
+    //             expenses_list:response.data.data,
+    //             itemsCountPerPage: response.data.per_page,
+    //             totalItemsCount: response.data.total,
+    //             activePage: response.data.current_page
+    //         });
+    //     });
+    // }
+
+    clickFunction = ()=>{
+        this.setState({
+            flag: true
         });
     }
 
@@ -39,50 +97,88 @@ class Expenses extends Component{
                 }
             }
         });
+
+        this.getChart();
+    }
+
+    getChart = () =>{
+        var id = localStorage.getItem('id');
+        axios.get('/api/pieChart/'+id)
+        .then(response=>{
+          console.log(response.data);
+            this.setState({chartData:response.data});
+        });
     }
 
     render(){
         if(localStorage.getItem('email') == null){
             return( <Redirect to={'/Login'} /> )
         }
+
+        if(this.state.flag == true){
+            return( <Redirect to={'/AddExpense'} /> )
+
+        }
         return(
             <div>
                 <Header />
-                <br></br>
-                <table className="table table-striped" style={{ width: "70%", marginLeft: "170px" }}>
-                <thead>
-                    <tr>
-                        <th scope="col"></th>
-                        <th scope="col">Category</th>
-                        <th scope="col">Amount</th>
-                        <th scope="col">Date</th>
-                        <th scope="col">Action</th>
+                
+                <div style={{ float: "left", overflow: "hidden", marginLeft: "70px", marginTop: "50px", width: "45%"}}>
 
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        this.state.expenses_list.map(exp=>{
-                           return(
-                            <tr key={exp.id}>
-                                <th scope="row"></th>
-                                    <td>{exp.category_name}</td>
-                                    <td>{exp.amount}</td>
-                                    <td>{exp.date}</td>
-                                    <td>
-                                        <Link to={`/EditExpense/${exp.id}`}>Edit</Link> | 
-                                        <a href="#" data-key={exp.id} onClick={this.onDelete}> Delete</a>
-                                    </td>
-                            </tr>
-                           )
+                    <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col"></th>
+                            <th scope="col">Category</th>
+                            <th scope="col" onClick={this.handleAmountSort}>Amount</th>
+                            <th scope="col" onClick={this.handleDateSort}>Date</th>
+                            <th scope="col">Action</th>
 
-                        })
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.expenses_list.sort(this.state.sort).map(exp=>{
+                            return(
+                                <tr key={exp.id}>
+                                    <th scope="row"></th>
+                                        <td>{exp.category_name}</td>
+                                        <td>{exp.amount}</td>
+                                        <td>{exp.date}</td>
+                                        <td>
+                                            <Link to={`/EditExpense/${exp.id}`}>Edit</Link> | 
+                                            <a href="#" data-key={exp.id} onClick={this.onDelete}> Delete</a>
+                                        </td>
+                                </tr>
+                            )
 
-                    }
+                            })
 
-                </tbody>
-                </table>
+                        }
 
+                    </tbody>
+                    </table>
+                    <button type="button" class="btn btn-secondary btn-circle btn-l" 
+                        style={{ float: "right", fontSize: "20px", borderRadius: "50%", width: "45px" }} onClick={this.clickFunction}>+</button> 
+
+                    <div>
+                        {/* <Pagination
+                            activePage = {this.state.activePage}
+                            itemsCountPerPage = {this.state.itemsCountPerPage}
+                            totalItemsCount = {this.state.totalItemsCount}
+                            pageRangeDisplayed = {this.state.pageRangeDisplayed}
+                            onChange = {this.handlePageChange}
+                            itemClass='page-item'
+                            linkClass = 'page-link'
+                            firstPageText = 'First'
+                            lastPageText = 'last'
+                        /> */}
+                    </div>
+                    
+                </div>
+                <div style={{ float: "left", overflow: "hidden", marginTop: "50px", marginLeft: "20px", width: "45%" }}>
+                    <PieChart dataParentToChild = {this.state.chartData}/>
+                </div>
             </div>
         )
     }
--- expensetracker_react_project/src/pages/PieChart.js
+++ expensetracker_react_project/src/pages/PieChart.js
@@ -4,85 +4,38 @@ import Header from './Header';
 import axios from 'axios';
 
 
-class PieChart extends Component {
 
-    constructor(){
-        super()
-        this.state={
-            categories:[],
-            labels: [],
-            datasets: [
-                {
-                  label: 'Rainfall',
-                  backgroundColor: [
-                    '#B21F00',
-                    '#C9DE00',
-                    '#2FDE00',
-                    '#00A6B4',
-                    '#6800B4'
-                  ],
-                  hoverBackgroundColor: [
-                  '#501800',
-                  '#4B5000',
-                  '#175000',
-                  '#003350',
-                  '#35014F'
-                  ],
-                  data: [55,75]
-                }
-              ]
-        }
-    }
+function PieChart(props) {
 
-    componentDidMount(){
-        var id = localStorage.getItem('id');
-        axios.get('/api/pieChart/'+id)
-        .then(response=>{
-            this.setState({categories:response.data});
-        });
-
-    }
-
-    // dataChart=()=>{
-    //   var category = this.state.categories;
-    //   console.log(category.length);
-    //   let names_list=[];
-    //   let sum_list=[];
-    //   for(var i=0; i<category.length;i++){
-    //     console.log(category[i]);
-    //     names_list.push(category[i].category_name);
-    //     sum_list.push(category[i].sum);
-    //   }
-    //   console.log(names_list);
-
-    // }
-
-    render() {
-        return (
-          <div>
-            <Header />
-            <div style={{ width: "700px", marginTop: "80px", marginLeft: "250px" }}>
-            <h2 style={{ float: "left", marginLeft: "150px", marginBottom: "25px" }}>Categories Pie Chart</h2>
-                <Pie
-                data={this.state}
-                options={{
-                    title:{
-                    display:true,
-                    fontSize:20
-                    },
-                    legend:{
-                    display:true,
-                    position:'right'
-                    }
-                }}
-                />
-            </div>
-    
-            
-          </div>
-        );
-      }
-    
+  const dataCat = props.dataParentToChild.map((data2) => { return data2.category_name })
+  const dataTot = props.dataParentToChild.map((data2) => { return data2.sum })
+  return (
+              <Pie
+                  data = {{
+                      labels:dataCat,
+                      datasets :[{
+                          label: 'Expenses',
+                          data:dataTot,
+                          backgroundColor: [
+                              'rgba(255, 241, 201)',
+                              'rgba(247, 183, 163)',
+                              'rgba(234, 95, 137)',
+                              'rgba(155, 49, 146)',
+                              'rgba(87, 22, 126)',
+                              'rgba(43, 11, 63)'
+                          ],
+                          // borderColor: [
+                          //     'rgba(255, 99, 132, 1)',
+                          //     'rgba(54, 162, 235, 1)',
+                          //     'rgba(255, 206, 86, 1)',
+                          //     'rgba(75, 192, 192, 1)',
+                          //     'rgba(153, 102, 255, 1)',
+                          //     'rgba(255, 159, 64, 1)'
+                          // ],
+                          borderWidth: 1
+                      }]
+                  }}
+              />
+      )
 }
-
 export default PieChart;
